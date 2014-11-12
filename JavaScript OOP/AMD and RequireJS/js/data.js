a7b383c25c2bf5cb52a21fea36c47cadbb568bd6
V45370 <vassilbb@gmail.com>
2014-11-12 18:50:03 +0200

    JS OOP
    HW

---
+++ JavaScript OOP/AMD and RequireJS/js/data.js
@@ -0,0 +1,20 @@
+define(function() {
+    'use strict';
+
+    return [{
+        id: 1,
+        name: "Doncho Minkov",
+        age: 24,
+        avatarUrl: "images/doncho.jpg"
+    }, {
+        id: 2,
+        name: "Ivaylo Kenov",
+        age: 25,
+        avatarUrl: "images/ivo.jpg"
+    }, {
+        id: 3,
+        name: "Nikolay Kostov",
+        age: 24,
+        avatarUrl: "images/niki.jpg"
+    }];
+});
